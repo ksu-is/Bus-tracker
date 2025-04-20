6b3f6e22557ecc808b0a184991cf258a71821172
Dcutie <dceasar@students.kennesaw.edu>
2025-04-20 13:26:19 -0400

    Update installment.py

--- installment.py
+++ installment.py
@@ -1,1 +1,3 @@
+Bus tracker is availabe on IOS and Android, providing real-time alerst to bus drivers when students are within walking distance of their pickup spot.
+  #installing
 pip install flask 
